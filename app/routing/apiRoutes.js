3348238f6cefd02bafbc87a84dfb2736cf46dfef
rebecca palmore <rebeccapalmore@Rebeccas-MacBook-Pro.local>
2017-05-06 07:09:40 -0500

    updating

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -40,7 +40,9 @@ module.exports = function(app) {
 
                 if (x != i) {
                     console.log("Difference with " + friendsData[x].name + ": " + sum);
-
+                    if (sum <= x) {
+                        console.log("Best match: " + friendsData[x].name);
+                    }
                 }
 
             }
@@ -51,19 +53,6 @@ module.exports = function(app) {
 }
 
 
-
-
-// Determine the user's most compatible friend using the following as a guide:
-
-// Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-
-// With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
-
-// Example:
-// User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-// User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
-
-// Total Difference: 2 + 1 + 2 = 5
 
 // The closest match will be the user with the least amount of difference.
 
